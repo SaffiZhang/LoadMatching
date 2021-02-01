3a2789ec30b925a4ab8e9199e0e58612f14cec15
Virola, Ziegfredo <ziegfredo@loadlink.ca>
2021-02-01 18:13:47 -0500

    Initial Development

---
+++ LoadLink.LoadMatching.Api.Test/RepostAll/RepostAllControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using LoadLink.LoadMatching.Api.Controllers;
+using LoadLink.LoadMatching.Api.Services;
+using LoadLink.LoadMatching.Api.Test.Setup;
+using LoadLink.LoadMatching.Application.RepostAll.Services;
+using LoadLink.LoadMatching.Persistence.Repositories.RepostAll;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LoadLink.LoadMatching.Api.Test.RepostAll
+{
+    public class RepostAllControllerTest
+    {
+        private readonly Mock<IHttpContextAccessor> _fakeHttpContextAccessor;
+        private readonly IUserHelperService _userHelper;
+        private readonly IRepostAllService _service;
+        private readonly RepostAllController _repostAllController;
+
+        public RepostAllControllerTest()
+        {
+            var userId = 1234;
+            _fakeHttpContextAccessor = new FakeContext().MockHttpContext(userId);
+
+            // integration            
+            var repository = new RepostAllRepository(new DatabaseFixture().ConnectionFactory);
+            _service = new RepostAllService(repository);
+
+            // controller
+            _userHelper = new UserHelperService(_fakeHttpContextAccessor.Object, null);
+            _repostAllController = new RepostAllController(_service, _userHelper);
+        }
+
+        [Fact]
+        public async Task RepostAllAsync()
+        {
+
+            // act
+            var actionResult = await _repostAllController.RepostAllAsync();
+
+            // assert
+            Assert.IsType<OkObjectResult>(actionResult);
+        }
+    }
+}
---
+++ LoadLink.LoadMatching.Api/Controllers/RepostAllController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using LoadLink.LoadMatching.Application.RepostAll.Services;
+using LoadLink.LoadMatching.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadLink.LoadMatching.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepostAllController : ControllerBase
+    {
+        private readonly IRepostAllService _repostAllService;
+        private readonly IUserHelperService _userHelperService;
+
+        public RepostAllController(IRepostAllService repostAllService,
+            IUserHelperService userHelperService)
+        {
+            _repostAllService = repostAllService;
+            _userHelperService = userHelperService;
+        }
+
+        [HttpGet("repost-all")]
+        public async Task<IActionResult> RepostAllAsync()
+        {
+            var custCd = ""; //_userHelperService.GetCustCd();
+            var userId = _userHelperService.GetUserId();
+
+            var result = await _repostAllService.RepostAllAsync(custCd, userId);
+            if (result == 0)
+                return NoContent();
+
+            return Ok(result);
+        }
+    }
+}
