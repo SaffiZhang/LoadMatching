1a05bc9c4562775da96c1d4779537719a424aaec
Virola, Ziegfredo <ziegfredo@loadlink.ca>
2021-02-03 12:45:35 -0500

    Freightmacthing-PDRatioController Initial Development

---
+++ LoadLink.LoadMatching.Application/PDRatio/Models/Commands/GetPDRatioCommand.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LoadLink.LoadMatching.Application.PDRatio.Models.Commands
+{
+    public class GetPDRatioCommand
+    {
+        public string VehicleType { get; set; }
+        public int VehicleTypeConverted { get; set; }
+        public string SrceSt { get; set; }
+        public string SrceCity { get; set; }
+        public string DestSt { get; set; }
+        public string DestCity { get; set; }
+    }
+}
