f1353a0cbd2c14995e61b9aa50e0c7f477bb673f
Mekhaeil, Vevian <Vevian.mekhaeil@transcore.ca>
2021-02-09 14:06:41 -0500

    DATLoadLead Controller Rewrite

---
+++ LoadLink.LoadMatching.Application/DATLoadLead/Repository/IDatLoadLeadRepository.cs
@@ -0,0 +1,12 @@
+﻿using LoadLink.LoadMatching.Domain.Procedures;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LoadLink.LoadMatching.Application.DATLoadLead.Repository
+{
+    public interface IDatLoadLeadRepository
+    {
+        Task<IEnumerable<UspGetDatLoadLeadResult>> GetListAsync(string custCD);
+        Task<IEnumerable<UspGetDatLoadLeadResult>> GetByPostingAsync(string custCD, int postingId);
+    }
+}
