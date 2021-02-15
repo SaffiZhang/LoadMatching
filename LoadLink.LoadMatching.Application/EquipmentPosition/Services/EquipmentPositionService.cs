8110a99bdb0f530e53a160c77a55cd1dd7e7be19
Mekhaeil, Vevian <Vevian.mekhaeil@transcore.ca>
2021-02-15 01:05:00 -0500

    EquipmentPosition Controller Rewrite

---
+++ LoadLink.LoadMatching.Application/EquipmentPosition/Services/EquipmentPositionService.cs
@@ -0,0 +1,38 @@
+﻿
+using AutoMapper;
+using LoadLink.LoadMatching.Application.EquipmentPosition.Models.Queries;
+using LoadLink.LoadMatching.Application.EquipmentPosition.Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadLink.LoadMatching.Application.EquipmentPosition.Services
+{
+    public class EquipmentPositionService : IEquipmentPositionService
+    {
+        private readonly IEquipmentPositionRepository _equipmentPositionRepository;
+        private readonly IMapper _mapper;
+
+        public EquipmentPositionService(IEquipmentPositionRepository equipmentPositionRepository, IMapper mapper)
+        {
+            _equipmentPositionRepository = equipmentPositionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<GetEquipmentPositionQuery>> GetListAsync(int token)
+        {
+            var result = await _equipmentPositionRepository.GetListAsync(token);
+
+            if (!result.Any())
+                return null;
+
+            return _mapper.Map<IEnumerable<GetEquipmentPositionQuery>>(result);
+        }
+
+        public async Task CreateAsync(int token)
+        {
+            await _equipmentPositionRepository.CreateAsync(token);
+        }
+
+    }
+}
---
+++ LoadLink.LoadMatching.Application/EquipmentPosition/Services/IEquipmentPositionService.cs
@@ -0,0 +1,12 @@
+﻿using LoadLink.LoadMatching.Application.EquipmentPosition.Models.Queries;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LoadLink.LoadMatching.Application.EquipmentPosition.Services
+{
+    public interface IEquipmentPositionService
+    {
+        Task<IEnumerable<GetEquipmentPositionQuery>> GetListAsync(int token);
+        Task CreateAsync(int token);
+    }
+}
