983210feb392df4f9691b03753e527314595dc2c
Mekhaeil, Vevian <Vevian.mekhaeil@transcore.ca>
2021-02-17 17:26:38 -0500

    Flag Controller Rewrite + StatrupHelper missing services injection.

---
+++ LoadLink.LoadMatching.Api/Validations/Flag/CreateFlagValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using LoadLink.LoadMatching.Application.Flag.Models.Commands;
+
+namespace LoadLink.LoadMatching.Api.Validations.Flag
+{
+    public class CreateFlagValidator : AbstractValidator<CreateFlagCommand>
+    {
+        public CreateFlagValidator()
+        {
+            RuleFor(x => x.LToken).NotNull()
+                .WithMessage("LToken is Mandatory.");
+            RuleFor(x => x.LToken).GreaterThan((short)0)
+                .WithMessage("Invalid LToken - must be greater than 0.");
+            RuleFor(x => x.EToken).NotNull()
+                .WithMessage("EToken is Mandatory.");
+            RuleFor(x => x.EToken).GreaterThan((short)0)
+                .WithMessage("Invalid EToken - must be greater than 0.");
+            RuleFor(x => x.ContactCustCD).NotNull()
+                .WithMessage("ContactCustCD is Mandatory.");
+            RuleFor(x => x.ContactCustCD).NotEmpty()
+                .WithMessage("ContactCustCD is Mandatory.");
+        }
+    }
+}
---
+++ LoadLink.LoadMatching.Application/Flag/Services/IFlagService.cs
@@ -0,0 +1,15 @@
+﻿using LoadLink.LoadMatching.Application.Flag.Models.Commands;
+using LoadLink.LoadMatching.Application.Flag.Models.Queries;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LoadLink.LoadMatching.Application.Flag.Services
+{
+    public interface IFlagService
+    {
+        Task<GetFlagQuery> GetAsync(string custCd, int Id);
+        Task<IEnumerable<GetFlagQuery>> GetListAsync(string custCd);
+        Task<CreateFlagCommand> CreateAsync(CreateFlagCommand createFlagCommand);
+        Task DeleteAsync(int id);
+    }
+}
